ed33539b076a1a582044e5bd7584def1c4d8b3ed
Siyuan Wang <siyuanwang@Siyuans-MacBook-Pro.local>
2019-09-02 16:24:44 +0800

    added feature, searching, deleting and updating

--- dbConnection/src/dbConnection/db_connection.java
+++ dbConnection/src/dbConnection/db_connection.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 
 public class db_connection {
 	
-	static class file {
+	static class oneLine {
 		String string1;
 		String string2;
 		String string3;
@@ -24,7 +24,7 @@ public class db_connection {
 		String string5;
 		String string6;
 		String string7;
-		public file () {
+		public oneLine () {
 			String string1 = "";
 			String string2 = "";
 			String string3 = "";
@@ -35,110 +35,119 @@ public class db_connection {
 		}
 	}
 	
+	private static final String file_path = "/Users/siyuanwang/Desktop/xingzhan/test.TXT";
 	public static void main(String args[]){  
 		Connection con = null;
 		Statement stmt = null;
+		//java.sql.PreparedStatement
+		//excuteBatch
 		String sql;
-		
+		BufferedReader reader = null;
+		ResultSet rs;
+		//BufferedReader reader2 = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver"); 
 			con = DriverManager.getConnection(  
 					//"jdbc:mysql://112.74.109.18:3306/rpmdb","rpmdb","a9NajZWz");  
 					"jdbc:mysql://112.74.109.184:3306/rpm","rpmdb","a9NajZWz");  
-			if (con != null) {
-                System.out.println("connection success!");
-            }
-			stmt =  con.createStatement();
-///////////////////////////////////////////////////////
-			//processing the query
-//			stmt =  con.createStatement();
-//			sql = "SELECT * FROM aassssssssssMyhome";
-//			ResultSet rs = stmt.executeQuery(sql);
-//			
-//			//printing resultSet
-//			while(rs.next()){
-//				int f = rs.getInt("forniture");
-//				int m = rs.getInt("members");
-//				int l = rs.getInt("location");
-//				String i = rs.getString("income");
-//				
-//				System.out.print("Forniture" + f);
-//                System.out.print(", Members: " + m);
-//                System.out.print(", location: " + l);
-//                System.out.print(", Income: " + i);
-//                System.out.print("\n");
-//				
-//			}
-//
-///////////////////////////////////////////////////////////////This is only for testing
+			if (con == null)
+				throw new java.lang.NullPointerException("connection is null");
 			
-//		  stmt =  con.createStatement();
-//		  sql =  "DROP TABLE IF EXISTS tableFortesting \n" +
-//		  		  "CREATE TABLE tableFortesting ( \n" +
-//				  "`s1` VARCHAR(255) DEFAULT NULL, \n" +
-//				  "`s2` VARCHAR(255) DEFAULT NULL, \n" +
-//				  "`s3` VARCHAR(255) DEFAULT NULL, \n" +
-//				  "`s4` VARCHAR(255) DEFAULT NULL, \n" +
-//				  "`s5` VARCHAR(255) DEFAULT NULL, \n" +
-//				  "`s6` VARCHAR(255) DEFAULT NULL, \n" +
-//				  "`s7` VARCHAR(255) DEFAULT NULL \n" +
-//				") ENGINE=InnoDB DEFAULT CHARSET=utf8";
-//		  System.out.println(sql);
-//		  stmt.execute(sql);  
-//		  //new table created
-//		  //System.out.println("------------------------------------------");
-		
-	    	//Reading forom file and writing to the database
-	      BufferedReader reader;
-	      reader = new BufferedReader(new FileReader("/Users/siyuanwang/Desktop/xingzhan/test.TXT")); ////stub filename
-	      String line = reader.readLine();
-	      int count = 0;
-	      while(line != null) {
-	    	  count ++;
-	    	  line = reader.readLine();
-	      }
+			stmt =  con.createStatement();
+			
+
+	    	//Reading from file and writing to the database
+			reader = new BufferedReader(new FileReader(file_path)); ////stub filename
+			String line = reader.readLine();
+			int count = 0;
+			while (line != null) {
+				count ++;
+				line = reader.readLine();
+			}
+			reader.close();
 	      
-	      BufferedReader reader2 = new BufferedReader(new FileReader("/Users/siyuanwang/Desktop/xingzhan/test.TXT")); //stub filename
-	      line = reader2.readLine();
-	      ArrayList<file> lines = new ArrayList<file> (count); //file = seven breaked up strings
+			reader = new BufferedReader(new FileReader(file_path)); //stub filename
+			line = reader.readLine();
+			//ArrayList<file> lines = new ArrayList<file> (count); //file = seven breaked up strings
 	      
-	      for(int i = 0; i < count; i++) {
-	    	  String[] array = new String[7];
-	    	  array = line.split(",");
-	    	  //assigning values for the object
-	    	  file temp = new file();
+			for (int i = 0; i < count; i++) {
+				String[] array = new String[7];
+				array = line.split(",");
+				//assigning values for the object
+				oneLine Row = new oneLine();
 	    	  
-	    	  temp.string1 = array [0]; ///process a query here      out.print(temp.string1 + ",");
-	    	  temp.string2 = array [1];
-	    	  temp.string3 = array [2]; 
-	    	  temp.string4 = array [3]; 
-	    	  temp.string5 = array [4];
-	    	  temp.string6 = array [5];
-	    	  temp.string7 = array [6];
-	    	  lines.add(temp); 
+				Row.string1 = array [0]; ///process a query here      out.print(temp.string1 + ",");
+				Row.string2 = array [1];
+				Row.string3 = array [2]; 
+				Row.string4 = array [3]; 
+				Row.string5 = array [4];
+				Row.string6 = array [5];
+				Row.string7 = array [6];
+				//lines.add(temp); 	
 	    	  
-	    	  sql =  "INSERT INTO table_for_testing(s1, s2, s3, s4, s5, s6, s7, modified_date) VALUES( '"
-	    			  + lines.get(i).string1 + "', '" +  lines.get(i).string2 + "', '" + lines.get(i).string3
-	    			  + "', '" + lines.get(i).string4 + "', '" + lines.get(i).string5 + "', '" + lines.get(i).string6
-	    			  + "', '" + lines.get(i).string7 + "', " + "NOW());";
-	    	  System.out.println(sql);
+				sql =  "INSERT INTO table_for_testing(s1, s2, s3, s4, s5, s6, s7, modified_date) VALUES( '"
+	    			  + Row.string1 + "', '" +  Row.string2 + "', '" + Row.string3
+	    			  + "', '" + Row.string4 + "', '" + Row.string5 + "', '" + Row.string6
+	    			  + "', '" + Row.string7 + "', " + "NOW());";
+				System.out.println(sql);
 	    
-	    	  stmt.execute(sql);
+				stmt.execute(sql);
 	    	  //System.out.println("Writing success!");
 			  //adding data into the table
-			  
-	    	  line = reader2.readLine();
-	      }
-    	  System.out.println("Done");
+
+				line = reader.readLine();
+			}
+			reader.close();
+			System.out.println("------------------ File Writing Done! ------------------");
 			
-			if (con != null) {
-				con.close();
-                System.out.println("connection closed");
+			
+			//////////////////////////////////////////search in the database///////////////////////////////
+			String column = "s6";
+			String nameOfTable = "table_for_testing";
+			String condition = "id = 10006";
+			sql = "SELECT " + column + " FROM " + nameOfTable + " WHERE " + condition;
+			//stmt.addBatch(sql);
+			rs = stmt.executeQuery(sql);
+			while(rs.next()){
+                String s6 = rs.getString("s6");
+                
+                System.out.println("s6: " + s6);
+                System.out.println("------------------ Data Retriving Done! ------------------");
             }
+			//////////////////////////////////////////deleting from the database///////////////////////////////
+			condition = "id = 10000";
+			sql = "DELETE FROM " + nameOfTable + " WHERE " + condition;
+			stmt.execute(sql);
+			System.out.println("------------------ Deleting Done ------------------");
+			//////////////////////////////////////////update in the database///////////////////////////////	
+			condition = "id = 10005";
+			sql = "UPDATE " + nameOfTable + " SET " + column + " = '    new content   '" + " WHERE " + condition;
+			stmt.execute(sql);
+//////////////////////////////////
+			sql = "SELECT " + column + " FROM " + nameOfTable + " WHERE " + condition;
+			//stmt.addBatch(sql);
+			rs = stmt.executeQuery(sql);
+			while(rs.next()){
+                String s6 = rs.getString("s6");
+                
+                System.out.println("s6: " + s6);
+                System.out.println("------------------ Data Updating Done! ------------------");
+            }
+			
+			
 			
 		} catch (Exception e) {
             // TODO Auto-generated catch block
-            e.printStackTrace();
+			e.printStackTrace();
+        } finally {
+        	try {
+				if (con != null) {
+					con.close();
+				}
+				reader.close();
+			} catch (Exception ne) {
+				
+			}
         }
 
 	}
